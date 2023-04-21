3ec6377c9cf1757fbf57236cf6e9ced9731d210d
Jacob Miller <millerjr1181@gmail.com>
2023-04-21 15:39:18 -0400

    finished basic requirements

--- weatherScript.js
+++ weatherScript.js
@@ -1,9 +1,7 @@
-// Weather API Key: 955f10e58acb14070de949096114de11
-// Example API Call: api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=955f10e58acb14070de949096114de11
 const search = document.querySelector("#SearchButton");
 search.addEventListener("click", () => {
   let location = document.querySelector("#location").value;
-  document.querySelector("#location").textContent = "";
+  document.querySelector("#location").value = "";
   fetch(
     `http://api.openweathermap.org/geo/1.0/zip?zip=${location},us&appid=955f10e58acb14070de949096114de11`
   )
@@ -13,14 +11,59 @@ search.addEventListener("click", () => {
       let geo = [];
       geo.push(place.lat);
       geo.push(place.lon);
-      document.querySelector("#place").textContent += cityName;
+      document.querySelector("#place").textContent = cityName;
       return geo;
     })
     .then((geo) => {
       fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${geo[0]}&lon=${geo[1]}&appid=955f10e58acb14070de949096114de11`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${geo[0]}&lon=${geo[1]}&appid=955f10e58acb14070de949096114de11&units=imperial`
       )
         .then((response) => response.json())
-        .then((weather) => console.log(JSON.stringify(weather)));
+        .then((weather) => {
+          const date = new Date(weather.dt * 1000);
+          console.log(weather);
+          document.querySelector("#locality").innerHTML = weather.name;
+          document.querySelector("#date").innerHTML =
+            date.getMonth() + 1 + " / " + date.getDate();
+          document.querySelector("#currentTemp").innerHTML =
+            weather.main.temp + "°F";
+          document.querySelector("#feelsLike").innerHTML =
+            weather.main.feels_like + "°F";
+          document.querySelector("#currentWeather").innerHTML =
+            weather.weather[0].description;
+          document.querySelector(
+            "#weatherIcon"
+          ).src = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
+          document.querySelector("#windSpeed").innerHTML = weather.wind.speed;
+          document.querySelector("#lo").innerHTML =
+            weather.main.temp_min + "°F";
+          document.querySelector("#hi").innerHTML =
+            weather.main.temp_max + "°F";
+        })
+        .catch((error) => {
+          console.log(error);
+          document.querySelector("#locality").innerHTML =
+            "Sorry, there was an error fetching the weather data";
+          document.querySelector("#date").innerHTML = "";
+          document.querySelector("#currentTemp").innerHTML = "";
+          document.querySelector("#feelsLike").innerHTML = "";
+          document.querySelector("#currentWeather").innerHTML = "";
+          document.querySelector("#windSpeed").innerHTML = "";
+          document.querySelector("#lo").innerHTML = "";
+          document.querySelector("#hi").innerHTML = "";
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+      document.querySelector("#place").textContent =
+        "Sorry, there was an error fetching the location data";
+      document.querySelector("#locality").innerHTML = "";
+      document.querySelector("#date").innerHTML = "";
+      document.querySelector("#currentTemp").innerHTML = "";
+      document.querySelector("#feelsLike").innerHTML = "";
+      document.querySelector("#currentWeather").innerHTML = "";
+      document.querySelector("#windSpeed").innerHTML = "";
+      document.querySelector("#lo").innerHTML = "";
+      document.querySelector("#hi").innerHTML = "";
     });
 });
